c32b47d17b4468694219b201d5aece3bad6b4861
shameer <shameerchagani@gmail.com>
2023-03-29 00:24:56 +0300

    updated version

--- routes/users.js
+++ routes/users.js
@@ -1,73 +1,76 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const bcrypt = require('bcryptjs');
-const passport = require('passport');
+const bcrypt = require("bcryptjs");
+const passport = require("passport");
 // Load User model
-const User = require('../models/User');
-const { forwardAuthenticated } = require('../config/auth');
+const User = require("../models/User");
+const { forwardAuthenticated } = require("../config/auth");
 
 // Login Page
-router.get('/login', forwardAuthenticated, (req, res) =>res.render('login'));
+router.get("/login", forwardAuthenticated, (req, res) => res.render("login"));
 
 // Register Page
-router.get('/register', forwardAuthenticated, (req, res) =>res.render('register'));
+router.get("/register", forwardAuthenticated, (req, res) =>
+  res.render("register")
+);
 
 // Register
-router.post('/register', (req, res) => {
+router.post("/register", (req, res) => {
   const { name, email, password, password2 } = req.body;
   let errors = [];
 
   if (!name || !email || !password || !password2) {
-    errors.push({ msg: 'Please enter all fields' });
+    errors.push({ msg: "Please enter all fields" });
   }
 
   if (password != password2) {
-    errors.push({ msg: 'Passwords do not match' });
+    errors.push({ msg: "Passwords do not match" });
   }
 
   if (password.length < 6) {
-    errors.push({ msg: 'Password must be at least 6 characters' });
+    errors.push({ msg: "Password must be at least 6 characters" });
   }
 
   if (errors.length > 0) {
-    res.render('register', {
+    res.render("register", {
       errors,
       name,
       email,
       password,
-      password2
+      password2,
     });
   } else {
-    User.findOne({ email: email }).then(user => {
+    User.findOne({ email: email }).then((user) => {
       if (user) {
-        errors.push({ msg: 'Email already exists' });
-        res.render('register', {
+        errors.push({ msg: "Email already exists" });
+        res.render("register", {
           errors,
           name,
           email,
           password,
-          password2
+          password2,
         });
       } else {
         const newUser = new User({
           name,
           email,
-          password
+          password,
         });
 
         bcrypt.genSalt(10, (err, salt) => {
           bcrypt.hash(newUser.password, salt, (err, hash) => {
             if (err) throw err;
             newUser.password = hash;
-            newUser.save()
-              .then(user => {
+            newUser
+              .save()
+              .then((user) => {
                 req.flash(
-                  'success_msg',
-                  'You are now registered and can log in'
+                  "success_msg",
+                  "You are now registered and can log in"
                 );
-                res.redirect('/users/login');
+                res.redirect("/users/login");
               })
-              .catch(err => console.log(err));
+              .catch((err) => console.log(err));
           });
         });
       }
@@ -76,21 +79,28 @@ router.post('/register', (req, res) => {
 });
 
 // Login
-router.post('/login', (req, res, next) => {
-  passport.authenticate('local', {
-    successRedirect: '/blogs',
-    failureRedirect: '/users/login',
+router.post("/login", (req, res, next) => {
+  passport.authenticate("local", {
+    successRedirect: "/blogs",
+    failureRedirect: "/users/login",
     failureFlash: true,
     author: req.body,
-    title: 'Nodejs Blog App'
+    title: "Nodejs Blog App",
   })(req, res, next);
 });
 
 // Logout
-router.get('/logout', (req, res) => { {title: 'Nodejs Blog Site'}
-  req.logout();
-  req.flash('success_msg', 'You are logged out');
-  res.redirect('/users/login');
+router.get("/logout", (req, res) => {
+  {
+    title: "Nodejs Blog Site";
+  }
+  req.logout((err) => {
+    if (err) {
+      console.log(err);
+    }
+  });
+  req.flash("success_msg", "You are logged out");
+  res.redirect("/users/login");
 });
 
 module.exports = router;
